5eb2364c283344b834b256730c45b807e656cc34
alive-gitee <121340354@qq.com>
2026-06-21 18:39:50 +0800

    opt: 仅chi_sim语言包(省15MB), SW install预抓取, 清除旧缓存

--- sw.js
+++ sw.js
@@ -1,6 +1,23 @@
-var CACHE='wc2026_v1';
-self.addEventListener('install',function(e){self.skipWaiting();});
-self.addEventListener('activate',function(e){e.waitUntil(clients.claim());});
+var CACHE='wc2026_v2';
+var FILES=[
+    'https://cdn.jsdelivr.net/npm/tesseract.js-data@4.0.0/4.0.0/chi_sim.traineddata.gz'
+];
+self.addEventListener('install',function(e){
+    self.skipWaiting();
+    e.waitUntil(
+        caches.open(CACHE).then(function(c){
+            return Promise.all(FILES.map(function(u){
+                return fetch(u).then(function(r){if(r.ok)c.put(u,r);});
+            })).catch(function(){});
+        })
+    );
+});
+self.addEventListener('activate',function(e){
+    e.waitUntil(Promise.all([
+        clients.claim(),
+        caches.keys().then(function(ks){return Promise.all(ks.filter(function(k){return k!==CACHE;}).map(function(k){return caches.delete(k);}));})
+    ]));
+});
 self.addEventListener('fetch',function(e){
     if(e.request.url.indexOf('tesseract')>=0||e.request.url.indexOf('jsdelivr.net/npm/tesseract')>=0){
         e.respondWith(
